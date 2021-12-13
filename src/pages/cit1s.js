2dcb0b2f34b79cf017e767a5b3b51f940da0e3a2
Michael Smyers <michael@smyers.net>
2021-12-13 01:11:06 -0800

    autocommit

--- src/pages/cit1s.js
+++ src/pages/cit1s.js
@@ -1,6 +1,11 @@
 import ProductPage from "../components/ProductPage";
+import Sitemap from "../sitemap";
 
 export default function Cit1s() {
+    if (!Sitemap.PRODUCTS['cit1s']) {
+        return <></>
+    }
+
     return (
         <ProductPage id={'cit1s'} />
     )
--- src/pages/cit1x.js
+++ src/pages/cit1x.js
@@ -1,6 +1,12 @@
 import ProductPage from "../components/ProductPage";
 
+import Sitemap from "../sitemap";
+
 export default function Cit1x() {
+    if (!Sitemap.PRODUCTS['cit1x']) {
+        return <></>
+    }
+
     return (
         <ProductPage id={'cit1x'} />
     )
